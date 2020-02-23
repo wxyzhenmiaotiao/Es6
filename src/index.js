3010159a03c6bf6c26a87775f40028c76c5d3984
wxyzhenmiaotiao <wxyzhenmiaotiao>
2020-02-23 20:26:06 +0800

    asd

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import App1 from './pages/221';
+import App1 from './pages/223';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App1 />, document.getElementById('root'));
